9c8600b376e8570d72793694e99ca76679b2418c
Tejas G <76802638+g-tejas@users.noreply.github.com>
2022-12-04 06:01:11 +0000

    required abit of input manipulation

---
+++ day4/1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+ 
+int main() {
+    string line;
+    int num_overlapping_pairs = 0;
+
+    while (getline(cin, line)) {
+    	stringstream line_ss(line);
+    	int a, b, c, d;
+    	line_ss >> a >> b >> c >> d;
+    	
+    	if ((a <= c) && (b >= d) || (c <= a) && (d >= b)) {
+    		num_overlapping_pairs++;
+    		// cout << "yes" << endl;
+    	} else {
+    		// cout << "no" << endl;
+    	}
+    }
+
+    cout << num_overlapping_pairs;
+}
