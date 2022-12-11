f205aa8b671d3fa3fce8dbaf798bdccfaa2e3073
Tejas G <ttejasgarrepally@gmail.com>
2022-12-11 15:48:28 +0800

    added part 2 stuff

---
+++ day11/2.cpp
@@ -0,0 +1,6 @@
+// use lcm of the divisibility tests, and apply modulo lcm to truncate the integer values so that they don't overflow
+// could also use BigInt if using javascript
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
